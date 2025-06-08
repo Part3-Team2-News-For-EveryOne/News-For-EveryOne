6fa07926c8ac751f6b63b42bf9e4bdbbd821e910
bokyung_kim <chloeee0228@gmail.com>
2025-06-08 21:04:42 +0900

    Feature: 세션 인증 방식 도입으로 권한 확인 - spring security에서 제공하는 session생성 도입

--- src/main/java/com/example/newsforeveryone/user/config/SecurityConfig.java
+++ src/main/java/com/example/newsforeveryone/user/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.newsforeveryone.user.config;
 
-import com.example.newsforeveryone.user.service.CustomUserDetailsService;
+import com.example.newsforeveryone.user.config.auth.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   private final CustomUserDetailsService userDetailService;
+  private final UserIdInjectionFilter userIdInjectionFilter;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -27,13 +29,15 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
     return http
         .csrf(csrf -> csrf.disable())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/", "/index.html", "/favicon.ico", "/assets/index-*.js", "/assets/index-*.css", "/api/users", "/api/users/login").permitAll()  //회원가입, 로그인은 허용 경로
             .anyRequest().authenticated())  //허용 경로를 제외한 모든 요청은 인증 필요
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
         .userDetailsService(userDetailService)
+        .addFilterAfter(userIdInjectionFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
