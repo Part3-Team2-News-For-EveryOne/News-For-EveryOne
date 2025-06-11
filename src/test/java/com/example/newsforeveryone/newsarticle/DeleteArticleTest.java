69493750c8307abf895c06e82ccdef0d6c1664ea
homea90 <homea90@naver.com>
2025-06-11 23:20:15 +0900

    Fix: CI/CD 파이프라인_github/workflows 수정

--- src/test/java/com/example/newsforeveryone/newsarticle/DeleteArticleTest.java
+++ src/test/java/com/example/newsforeveryone/newsarticle/DeleteArticleTest.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DeleteArticleTest111 extends IntegrationTestSupport {
+public class DeleteArticleTest extends IntegrationTestSupport {
 
     @PersistenceContext
     EntityManager em;
