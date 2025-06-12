e3ecc31d3b46a1930086c6b8164109ff99529cee
sangyeobhan <sangyeobhandev@gmail.com>
2025-06-12 09:10:18 +0900

    Feat: FakeComment삭제 Comment랑 통합

--- src/main/java/com/example/newsforeveryone/common/exception/ErrorCode.java
+++ src/main/java/com/example/newsforeveryone/common/exception/ErrorCode.java
@@ -25,7 +25,9 @@ public enum ErrorCode {
     COMMENT_ALREADY_DELETED(410, "CM02", "이미 삭제된 댓글입니다."),
 
     // 기사 관련
-    ARTICLE_NOT_FOUND(404,"A03", "기사를 찾을 수 없습니다"),
+    ARTICLE_NOT_FOUND(404,"A01", "기사를 찾을 수 없습니다"),
+    ARTICLE_METRIC_NOT_FOUND(404,"A02", "해당 기사의 메트릭을 찾을 수 없습니다."),
+
     // 공통
     INVALID_REQUEST(400, "C01", "잘못된 요청입니다"),
     INTERNAL_SERVER_ERROR(500, "C02", "서버 내부 오류가 발생했습니다");
